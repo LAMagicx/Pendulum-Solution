e640c8d5a473c878ae1b3b3a93c7f9ccd408fa2d
Samuel <sam200133@gmail.com>
2021-05-15 15:06:43 +0200

    added exact solution

--- solver.py
+++ solver.py
@@ -2,24 +2,35 @@ import numpy as np
 import scipy.integrate as spi
 from RK import RK1, RK2
 
-def equation1(fun, args, theta_0=[1,1], t_max=np.pi*2, n=50):
+
+def equation1(fun, args, theta_0=[1, 1], t_max=np.pi*2, n=50):
     t_0 = 0
     t_1 = t_max
     t = (t_0, t_1)
 
-    theta = spi.solve_ivp(lambda t, y: fun(t, y, args[0], args[1], args[2], args[3]), t, theta_0, t_eval=np.linspace(t_0, t_1, n))
+    theta = spi.solve_ivp(lambda t, y: fun(
+        t, y, args[0], args[1], args[2], args[3]), t, theta_0, t_eval=np.linspace(t_0, t_1, n))
 
     #((theta.y[0] + np.pi) % (2 * np.pi)) - np.pi
-    return theta.t, theta.y[0,:]
+    return theta.t, theta.y[0, :]
 
 
-def equation2(fun, args, theta_0=[1,1], t_max=np.pi*2, n=50):
-    theta = RK1(lambda t, y: fun(t, y, args[0], args[1], args[2], args[3]), np.array([0, t_max]), np.array(theta_0), t_eval=np.linspace(0, t_max, n))
+def equation2(fun, args, theta_0=[1, 1], t_max=np.pi*2, n=50):
+    theta = RK1(lambda t, y: fun(t, y, args[0], args[1], args[2], args[3]), np.array(
+        [0, t_max]), np.array(theta_0), t_eval=np.linspace(0, t_max, n))
 
-    return np.linspace(0, t_max, n-3), theta[0,:]
+    return np.linspace(0, t_max, n-3), theta[0, :]
 
 
-def equation3(fun, args, theta_0=[1,1], t_max=np.pi*2, n=50):
-    theta = RK2(lambda t, y: fun(t, y, args[0], args[1], args[2], args[3]), np.array([0, t_max]), np.array(theta_0), t_eval=np.linspace(0, t_max, n))
+def equation3(fun, args, theta_0=[1, 1], t_max=np.pi*2, n=50):
+    theta = RK2(lambda t, y: fun(t, y, args[0], args[1], args[2], args[3]), np.array(
+        [0, t_max]), np.array(theta_0), t_eval=np.linspace(0, t_max, n))
 
-    return np.linspace(0, t_max, n-3), theta[0,:]
+    return np.linspace(0, t_max, n-3), theta[0, :]
+
+
+def equation4(fun, args, theta_0=[1, 1], t_max=np.pi*2, n=50):
+    t_span = np.linspace(0, t_max, n-3)
+    theta = fun(t_span, theta_0[0])
+    print(theta)
+    return t_span, theta
